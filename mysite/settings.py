4046b66f64bc85478352b39e3aa737e57d706f74
Kailash <58307685+KailashGanesh@users.noreply.github.com>
2020-09-28 21:41:48 +0530

    pythonanywhere setting cheange

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +26,7 @@ SECRET_KEY = '#1f!8n_sy$7zqvg%m=_+tyjk13%l+ls#lzv@bhy0du%6_d%eug'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kailash.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/kailash/Learning_django/static'
